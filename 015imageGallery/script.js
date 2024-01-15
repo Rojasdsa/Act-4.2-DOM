74cbcf32fa3dca5e2b3630986761ffddda7aa060
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-16 00:43:01 +0100

    Actividad finalizada

--- 015imageGallery/script.js
+++ 015imageGallery/script.js
@@ -1,2 +1,15 @@
 'use strict'
 
+thumbs.onclick = function(event) {
+    let thumbnail = event.target.closest('a');
+
+    if (!thumbnail) return;
+    showThumbnail(thumbnail.href, thumbnail.title);
+    event.preventDefault();
+  }
+
+  // Sustituye los atributos por los de la <img> clickada
+  function showThumbnail(href, title) {
+    largeImg.src = href;
+    largeImg.alt = title;
+  }
