1d3298563d3782e75d63c7b918a5bc8775e9ae7c
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-15 19:16:51 +0100

    Actividad finalizada

--- 006handlers/script.js
+++ 006handlers/script.js
@@ -1,2 +1,24 @@
 'use strict'
 
+// Funciona y muestra "1"
+button.addEventListener("click", () => alert("1"));
+
+/* No funciona. Para remover el handler necesitamos pasar exactamente 
+la función que fue asignada. 
+*/ 
+button.removeEventListener("click", () => alert("1"));
+
+// Funciona y muestra "2". Funciona independientemente.
+button.onclick = () => alert(2);
+
+
+/* 
+Para que removeEventListener funciones habría que crear una función:
+
+function handler() {
+  alert(1);
+}
+
+button.addEventListener("click", handler);
+button.removeEventListener("click", handler);
+*/
