dfe62936947d92bd2f3d7b49cfcbc97f0c892ca4
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-15 19:45:53 +0100

    Actividad finalizada

--- 007collapsableMenu/script.js
+++ 007collapsableMenu/script.js
@@ -1,2 +1,8 @@
 'use strict'
 
+let menu = document.getElementById('criptos');
+let headerMenu = menu.querySelector('.header');
+
+headerMenu.onclick = function() {
+  menu.classList.toggle('open');
+};
