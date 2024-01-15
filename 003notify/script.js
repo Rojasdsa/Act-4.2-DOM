5ec46331dcf1d08abbd7bab02ffedaa3eb8cc64a
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-15 18:16:36 +0100

    Actividad finalizada

--- 003notify/script.js
+++ 003notify/script.js
@@ -1,2 +1,34 @@
 'use strict'
 
+function showNotification({ top = 0, right = 0, className, html }) {
+
+    let notification = document.createElement('div');
+
+    // Nombre de la clase
+    notification.className = "notification";
+    if (className) {
+        notification.classList.add(className);
+    }
+
+    // Estilo de la calse
+    notification.style.top = top + 'px';
+    notification.style.right = right + 'px';
+
+    // Mensaje HTML al final del body
+    notification.innerHTML = html;
+    document.body.append(notification);
+
+    // Borra el hello mostrado tras 1,5 segundos
+    setTimeout(() => notification.remove(), 1500);
+}
+
+// Mostrará un hello cada 2 segundos
+let i = 1;
+setInterval(() => {
+    showNotification({
+        top: 10,
+        right: 10,
+        html: 'Hello ' + i++,
+        className: "welcome"
+    });
+}, 2000);
