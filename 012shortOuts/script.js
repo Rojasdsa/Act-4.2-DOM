c30db298651ec460ec3cbb1dc6d8662c49d9bfc7
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-15 20:39:59 +0100

    Actividad finalizada

--- 012shortOuts/script.js
+++ 012shortOuts/script.js
@@ -1,2 +1,33 @@
 'use strict'
 
+function runOnKeys(func, ...codes) {
+    let pressed = new Set();
+
+    document.addEventListener('keydown', function (event) {
+        pressed.add(event.code);
+
+        for (let code of codes) {
+            if (!pressed.has(code)) {
+                return;
+            }
+        }
+ 
+        /* Para evitar teclas "pegadas", reseteamos el estado pressed
+        si el usuario quiere usa el atajo de nuevo, permitámoslo */
+        pressed.clear();
+
+        func();
+    });
+
+    document.addEventListener('keyup', function (event) {
+        pressed.delete(event.code);
+    });
+
+}
+
+// Teclas definidas
+runOnKeys(
+    () => alert("Tú has provocado este alert"),
+    "KeyQ",
+    "KeyW"
+);
