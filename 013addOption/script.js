720bedf3ed72c2750f5391d970b3a4e176c346e9
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-15 20:49:25 +0100

    Actividad finalizada

--- 013addOption/script.js
+++ 013addOption/script.js
@@ -1,2 +1,12 @@
 'use strict'
 
+// Muestra la opción seleccionada con "selected"
+let selectedOption = genres.options[genres.selectedIndex];
+alert(selectedOption.value);
+
+// Se añade una nueva option para elegir
+let newOption = new Option("Classic", "classic");
+genres.append(newOption);
+
+// Selecciona la opción creada antes de desplegar el menú
+newOption.selected = true;
