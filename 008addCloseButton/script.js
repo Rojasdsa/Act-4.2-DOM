ba3395e2e42acc29cabbfdc4ce5556d303d944d3
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-15 20:04:10 +0100

    Actividad finalizada

--- 008addCloseButton/script.js
+++ 008addCloseButton/script.js
@@ -1,2 +1,12 @@
 'use strict'
 
+let conts = document.querySelectorAll('.cont');
+
+for (let cont of conts) {
+
+    // Añadimos el botón sin funcionalidad
+    cont.insertAdjacentHTML("afterbegin", '<button class="remove-button">[x]</button>');
+    
+    // Button debe convertirse en el primer child de cont
+    cont.firstChild.onclick = () => cont.remove();
+}
